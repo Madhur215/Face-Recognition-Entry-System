d79a323e85c9511e18b2094beade25de44cc5ccd
Madhur215 <vashistha.madhur2@gmail.com>
2020-04-16 13:41:12 +0530

    Complete close entry and fix video bugs

--- DetectImage.py
+++ DetectImage.py
@@ -10,64 +10,50 @@ names = { 0: 'niharika', 1 : 'madhur', 2: 'v', 3: 'l'}
 
 class detect_image:
 
-	def __init__(self):
+	def __init__(self, recordSheet, attendenceSheet):
+		self.recordSheet = recordSheet
+		self.attendenceSheet = attendenceSheet
 		self.cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 		self.recognizer = cv2.face.LBPHFaceRecognizer_create()
 		self.recognizer.read('trained.yml')
 		self.students_marked = []
+		self.recordWorkbook = load_workbook(filename=recordSheet)
+		self.attendenceWorkbook = load_workbook(filename=attendenceSheet)
 
-	def identify(self, recordSheet, attendenceSheet):
+	def identify(self):
 		cap = cv2.VideoCapture(0)
 		f = 0
+		pred_name = "None"
 		while True:
 			_, frame = cap.read()
 			gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 			equalize = cv2.equalizeHist(gray_frame)
 			image = cv2.medianBlur(equalize, 3)
 			face = self.cascade.detectMultiScale(image, 1.1, 5)
-
+			
 			for x, y, w, h in face:
 				cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 0), 4)
 				label, conf = self.recognizer.predict(gray_frame[y:y+h, x:x+w])
-				# print(names.get(label))
 				p_name = names.get(label)
-
+				pred_name = p_name
 				cv2.imshow("Recognizing Face", frame)
 				flag = cv2.waitKey(1) & 0xFF
 				f += 1
 				if f > 20:
-					self.markEntry(p_name, recordSheet, attendenceSheet)
+					# if n == 0:
+					# 	self.markEntry(p_name)
+					# else:
+					# 	self.close_entry(p_name)
 					cap.release()
-					break	
+					return p_name
+					# break	
+		# return pred_name
 
-			
-	def markEntry(self, p_name, recordSheet, attendenceSheet):
-		if p_name in attendenceSheet:
-			print("Entry already marked!")
-		else:
-			self.students_marked.append(p_name)
-			recordWorkbook = load_workbook(filename=recordSheet)
-			sheet = recordWorkbook.active
-			for values in sheet.iter_rows(min_row = 1,
-										min_col = 1,
-										max_col = 3,
-										values_only = True):
-				rec_name = values[0]
-				if rec_name.lower() == p_name:
-					branch = values[1]
-					year = values[2]
-					self.writeEntry(p_name, branch, year, attendenceSheet)
-
-	def print_rows(self, sheet):
-		for row in sheet.iter_rows(values_only=True):
-			print(row)
-
-	def writeEntry(self, p_name, branch, year, attendenceSheet):
+	def writeEntry(self, p_name, branch, year):
 		# num_students = len(self.students_marked)
 		# print(self.students_marked)
 		# print(branch, year)
-		attendenceWorkbook = load_workbook(filename=attendenceSheet)
-		sheet = attendenceWorkbook.active
+		sheet = self.attendenceWorkbook.active
 		sheet.insert_rows(idx=2, amount=1)
 		sheet["A2"] = p_name
 		sheet["B2"] = year
@@ -76,13 +62,42 @@ class detect_image:
 		curr_time = time.strftime("%H:%M:%S")
 		# print(curr_time)
 		sheet["D2"] = curr_time
-		
-		self.print_rows(sheet)
-		attendenceWorkbook.save(attendenceSheet)
+		self.attendenceWorkbook.save(self.attendenceSheet)
+
+			
+	def markEntry(self, p_name):
+		if p_name in self.students_marked:
+			print("Entry already marked!")
+		else:
+			self.students_marked.append(p_name)
+			sheet = self.recordWorkbook.active
+			for values in sheet.iter_rows(min_col = 1,
+										max_col = 3,
+										values_only = True):
+				rec_name = values[0]
+				if rec_name.lower() == p_name:
+					branch = values[1]
+					year = values[2]
+					self.writeEntry(p_name, branch, year)
 
 
-
-
+	def close_entry(self, p_name):
+		if not p_name in self.students_marked:
+			print("Entry not marked!")
+		else:
+			sheet = self.attendenceWorkbook.active
+			index = 0
+			for values in sheet.iter_rows(values_only = True):
+				rec_name = values[0]
+				index += 1
+				if rec_name.lower() == p_name:
+					time = datetime.now()
+					curr_time = time.strftime("%H:%M:%S")
+					sheet["E" + str(index)] = curr_time
+					print("Entry closed!") 
+					self.attendenceWorkbook.save(self.attendenceSheet)
+					self.students_marked(p_name)					
+					break
 
 
 
--- main.py
+++ main.py
@@ -5,6 +5,15 @@ import os
 import cv2
 from train import train_images as tr
 from DetectImage import detect_image as detect
+import tkinter as tk
+
+root = tk.Tk()
+root.geometry('300x200')
+
+root.title('FARAS')
+frame = tk.Frame(root, bg="#FFF")
+frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+d = detect('Record.xlsx', 'attendence.xlsx')
 
 def train_recognizer():
 	recognizer = cv2.face.LBPHFaceRecognizer_create()
@@ -13,10 +22,33 @@ def train_recognizer():
 	image_id = np.array(image_id)
 	recognizer.train(images, image_id)
 	recognizer.save('trained.yml')
-if not os.path.isfile('trained.yml'):
-	train_recognizer()
 
-d = detect()
-d.identify('Record.xlsx', 'attendence.xlsx')
 
+def mark_entry():
+	p_name = d.identify()
+	print(p_name)
+	d.markEntry(p_name)
+
+def close_entry():
+	p_name = d.identify()
+	print(p_name)
+	d.close_entry(p_name)
+
+mark_entry_button = tk.Button(root, text="Mark Entry", command=mark_entry)
+mark_entry_button.place(relx=0.2, rely=0.5)
+close_entry_button = tk.Button(root, text="close Entry",command = close_entry)
+close_entry_button.place(relx=0.6, rely=0.5)
+
+if __name__ == '__main__':
+	if not os.path.isfile('trained.yml'):
+		train_recognizer()
+	root.mainloop()
+	# while True:
+	# 	st = input()
+	# 	if st == 'exit':
+	# 		break
+	# 	elif st == 'm':
+	# 		mark_entry()
+	# 	else:
+	# 		close_entry()
 
